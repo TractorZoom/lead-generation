6bf6a6688ddcd55559fe09389078b18684083bfc
Hank Mandsager <hmandsager@yahoo.com>
2024-11-12 17:10:44 -0600

    fix: a lot of category updates

--- src/transformation/category.py
+++ src/transformation/category.py
@@ -2,6 +2,7 @@
 
 import logging
 
+import numpy as np
 import polars as pl
 from sentence_transformers import SentenceTransformer, util
 
@@ -10,19 +11,20 @@ from src.utils.io import read_from_databricks
 SENTENCE_MODEL = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 MAX_CHARACTERS_IN_ACROYNM = 3
+MIN_SIMILARITY_DEVIATION = 0.02
+BEST_SCORE_THRESHOLD = 0.5
 
 MAKE_MODEL_QUERY = """SELECT m2.id
      , m1.name AS make
      , m2.model
      , cat.name AS category
      , sub.name AS subcategory
-     , COUNT(l.id) AS total_units
+     , COUNT(ed.id) AS total_units
 FROM prod_mysql.silver_tractor_prod.make m1
 JOIN prod_mysql.silver_tractor_prod.make_model m2 ON m1.id = m2.make_id
 JOIN prod_mysql.silver_tractor_prod.lot_category cat ON cat.id = m2.category
 JOIN prod_mysql.silver_tractor_prod.lot_subcategory sub ON sub.id = m2.subcategory
-JOIN prod_mysql.silver_tractor_prod.lots l ON l.category_id = cat.id
-                                           AND l.subcategory_id = sub.id
+JOIN prod_mysql.silver_tractor_prod.equipment_details ed ON ed.make_model_id = m2.id
 WHERE m2.status = 1
 GROUP BY ALL
 ORDER BY total_units DESC"""
@@ -75,9 +77,9 @@ class CleanMakeModelData:
             group=group,
             use_semantic_check=True,
         )
-        if exact_match:
-            return exact_match[0]
 
+        if exact_match and exact_match[0]["best_fit_reason"] == "Exact Match":
+            return exact_match[0]
         # check for acronym in make
         if len(make) <= MAX_CHARACTERS_IN_ACROYNM:
             synonym_make = self.make_synonym_list(make)
@@ -87,14 +89,40 @@ class CleanMakeModelData:
                 group=group,
                 use_semantic_check=True,
             )
-            if match_check:
+            if match_check and match_check[0]["best_fit_reason"] in [
+                "Acronym",
+                "Exact Match",
+            ]:
                 return match_check[0]
 
         # check for most most likely based on aggregated data
         if self.aggregated_data.shape[0] > 0:
-            return self.check_aggregated_data(make, model, group)
+            aggregated_check = self.check_aggregated_data(make, model, group)
+            if aggregated_check and aggregated_check["category"] != "Unknown":
+                return aggregated_check
 
-        return {}
+        # check for semantic match
+        if group:
+            semantic_check = self._check_match(
+                synonym_make if synonym_make else make,
+                model,
+                group=group,
+                use_semantic_check=True,
+            )
+            if semantic_check and semantic_check[0]["best_fit_reason"].startswith(
+                "Semantic"
+            ):
+                return semantic_check[0]
+
+        # check for best guess based on group
+        if group:
+            best_guess = self.get_best_guess_cat_subcat(
+                synonym_make if synonym_make else make, model, group
+            )
+            if best_guess and best_guess["category"] != "Unknown":
+                return best_guess
+
+        return exact_match[0]
 
     def create_aggregated_data(
         self,
@@ -141,22 +169,100 @@ class CleanMakeModelData:
         self.aggregated_data = pl.DataFrame(temp_data)
 
     def _semantic_matching(self, group: str, group_pl: pl.DataFrame) -> dict:
-        subcats = group_pl["subcategory"].unique().to_list()
+        subcats = (
+            group_pl.with_columns(
+                (pl.col("category") + "-" + pl.col("subcategory")).alias("combined")
+            )
+            .select("combined")
+            .unique()
+            .to_series()
+            .to_list()
+        )
         best_score = -1
         best_subcat = ""
+        best_fit_reason = ""
+        scores = []
         group_embedding = SENTENCE_MODEL.encode(group, convert_to_tensor=True)
         for subcat in subcats:
             embedding = SENTENCE_MODEL.encode(subcat, convert_to_tensor=True)
             sim_score = util.pytorch_cos_sim(group_embedding, embedding)
+            scores.append(sim_score.item())
             if sim_score > best_score:
-                best_score = sim_score
-                best_subcat = subcat
+                best_score = sim_score.item()
+                best_subcat = "-".join(subcat.split("-")[1:])
+                best_fit_reason = "Semantic - Best Match"
+
+        if best_score < BEST_SCORE_THRESHOLD:
+            best_subcat = group_pl["subcategory"][0]
+            best_fit_reason = "Semantic - Most Frequent (No Good Match)"
+
+        # check if best score is very similar to the other scores
+        try:
+            second_best_score = sorted(scores)[-2]
+        except (
+            IndexError
+        ):  # Case where the same equipment is listed twice in a subcategory
+            second_best_score = 1
+        if best_score - second_best_score < MIN_SIMILARITY_DEVIATION:
+            best_subcat = group_pl["subcategory"][0]
+            best_fit_reason = "Semantic - Most Frequent (No Clear Best Match)"
+
         best_fit = group_pl.filter(pl.col("subcategory") == best_subcat)
+
         return {
             "make": best_fit["make"][0],
             "model": best_fit["model"][0],
             "category": best_fit["category"][0],
             "subcategory": best_fit["subcategory"][0],
+            "best_fit_reason": best_fit_reason,
+            "best_fit_score": best_score,
+        }
+
+    def get_best_guess_cat_subcat(self, make: str, model: str, group: str) -> dict:
+        """Get the most common category and subcategory for a group.
+
+        Parameters
+        ----------
+        make: str
+            The make of the equipment.
+        model: str
+            The model of the equipment.
+        group : str
+            The group of the equipment.
+
+        Returns
+        -------
+        dict
+            The most common category and subcategory for the group.
+
+        """
+        group_category = (
+            self.aggregated_data.filter(
+                pl.col("group") == group,
+            )
+            .filter(pl.col("category") != "Unknown")
+            .group_by("category")
+            .agg(pl.count("category").alias("count"))
+            .sort("count", descending=True)
+            .head(1)
+        )["category"][0]
+        group_subcategory = (
+            self.aggregated_data.filter(
+                pl.col("group") == group,
+            )
+            .filter(pl.col("subcategory") != "Unknown")
+            .group_by("subcategory")
+            .agg(pl.count("subcategory").alias("count"))
+            .sort("count", descending=True)
+            .head(1)
+        )["subcategory"][0]
+        return {
+            "make": make,
+            "model": model,
+            "category": group_category,
+            "subcategory": group_subcategory,
+            "best_fit_reason": "No Match - Estimate Cat/Subcat",
+            "best_fit_score": -1,
         }
 
     def check_aggregated_data(self, make: str, model: str, group: str) -> dict:
@@ -187,6 +293,8 @@ class CleanMakeModelData:
                 "model": match["model"][0],
                 "category": match["category"][0],
                 "subcategory": match["subcategory"][0],
+                "best_fit_reason": "Exact Match",
+                "best_fit_score": 1,
             }
 
         # Assume there are no good matches, get common cat/subcat\
@@ -217,12 +325,15 @@ class CleanMakeModelData:
             "model": model,
             "category": group_category,
             "subcategory": group_subcategory,
+            "best_fit_reason": "Aggregated Most Likely",
+            "best_fit_score": -1,
         }
 
     def _check_match(
         self,
         make: str,
         model: str,
+        best_match_reason: str = "",
         *,
         group: str = "",
         use_semantic_check: bool = False,
@@ -235,6 +346,11 @@ class CleanMakeModelData:
             The make of the equipment.
         model : str
             The model of the equipment.
+        group : str
+            The dealer designated equipment group. This is used in semantic check to
+            identify most similar subcategory.
+        best_match_reason : str
+            The reason for the best match. This is used in the output.
         group : str
             The dealer designated equipment group. This is used in semantic check to
             identify most similar subcategory.
@@ -253,16 +369,31 @@ class CleanMakeModelData:
             (pl.col("make").str.to_lowercase() == make.lower())
             & (pl.col("model").str.to_lowercase() == model.lower()),
         )
-        if use_semantic_check and group and exact_match.shape[0] > 0:
+        # check for acronym in make
+        if len(make) <= MAX_CHARACTERS_IN_ACROYNM:
+            synonym_make = self.make_synonym_list(make)
+            return self._check_match(
+                synonym_make,
+                model,
+                group=group,
+                use_semantic_check=True,
+                best_match_reason="Acronym",
+            )
+
+        if use_semantic_check and group and exact_match.shape[0] > 1:
             return [self._semantic_matching(group, exact_match)]
 
         if exact_match.shape[0] > 0:
+            if not best_match_reason:
+                best_match_reason = "Exact Match"
             return [
                 {
                     "make": make,
                     "model": model,
                     "category": exact_match["category"][i],
                     "subcategory": exact_match["subcategory"][i],
+                    "best_fit_reason": best_match_reason,
+                    "best_fit_score": 1,
                 }
                 for i in range(exact_match.shape[0])
             ]
@@ -272,6 +403,8 @@ class CleanMakeModelData:
                 "model": model,
                 "category": "Unknown",
                 "subcategory": "Unknown",
+                "best_fit_reason": "No Match",
+                "best_fit_score": -1,
             },
         ]
 
