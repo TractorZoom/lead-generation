2f1f4c79f5e9732a5f2cbadd1ea70e54af49846d
Hank Mandsager <hmandsager@yahoo.com>
2024-12-17 13:24:01 -0600

    fix: correcting ave-plp sales history query

--- discovery/ave-plp/sales-history.sql
+++ discovery/ave-plp/sales-history.sql
@@ -1,9 +1,9 @@
 SELECT Id
      , Anvil__Account__c
      , Anvil__FiscalYear__c
-     , Anvil__PartsSales__c
-     , Anvil__ServiceSales__c
-     , Anvil__RentalSales__c
-     , Anvil__WholegoodSales__c
-     , Anvil__TotalSales__c
+     , Anvil__e_Parts2__c
+     , Anvil__e_Service2__c
+     , Anvil__e_Rental__c
+     , Anvil__e_Sales__c
+     , Anvil__e_Total_Rev__c
 FROM Anvil__Account_Sales_History__c 
